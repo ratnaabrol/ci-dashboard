44a76225f9131a536c7b667c1425f02c4da11309
ahmedelsayed93 <ahmed.elsayed93@eng-st.cu.edu.eg>
2017-07-15 19:02:21 +0200

    major changes

---
+++ repository.py
@@ -0,0 +1,82 @@
+from travis.client import Client
+from tools import Tools
+
+class Repository:
+    def __init__(self, slug):
+        self.slug = slug.replace('/', '%2F')
+        self.travis_url = "https://travis-ci.org/{slug}".format(slug=slug)
+        self.github_url = "http://github.com/{slug}".format(slug=slug)
+        self.__tools = Tools()
+        self.__config = self.__tools.read_config()
+        self.__travis_client = Client(self.__config['travis_token'])
+    
+    def info(self):
+        repo = self.__travis_client.repo(self.slug).json()
+        return self.__repoParser(repo)
+
+    def last_build(self):
+        builds = self.__travis_client.builds(self.slug, limit=1).json()['builds']
+        if builds:
+            last_build = builds[0]
+            return self.__buildParser(last_build)
+        else:
+            return None
+        
+    def branches(self):
+        branches = self.__travis_client.branches(self.slug, exists_on_github='true').json()
+        return [branch['name'] for branch in branches['branches']]
+    
+    def trigger_build(self, branch):
+        data = {"request": {"branch": branch}}
+        self.__travis_client.trigger_build(self.slug, data)
+    
+    def restart_build(self, buildid):
+        self.__travis_client.restart_build(buildid)
+    
+    def cancel_build(self, buildid):
+        self.__travis_client.cancel_build(buildid)
+
+    def __repoParser(self, repo):
+        data = {
+            "id": repo['id'],
+            "name": repo['name'],
+            "slug": repo['slug'],
+            "owner": repo['owner']['login'],
+            "url": self.travis_url,
+            "default_branch": repo['default_branch']['name'],
+            "branches": self.branches()
+        }
+        return data
+
+
+    def __buildParser(self, build):
+        commit_url = self.github_url + "/commit/{sha}"
+        build_url = self.travis_url + "/builds/{id}"
+
+        data = {
+            "id": build['id'],
+            "number": build['number'],
+            "state": build['state'].upper(),
+            "url": build_url.format(id=build['id']),
+            "branch": build['branch']['name'],
+            "commit_sha": build['commit']['sha'][:7],
+            "commit_author": build['commit']['author']['name'],
+            "commit_url": commit_url.format(sha=build['commit']['sha'])
+        }
+
+        if build['event_type'] == 'pull_request':
+            data['event'] = 'Pull Request #{}'.format(build['pull_request_number'])
+            data['event_title'] = build['pull_request_title']
+        else:
+            data['event'] = build['event_type'].title()
+            data['event_title'] = build['commit']['message']
+
+        if build['state'] == 'started':
+            time_event = 'Running for {}'.format(self.__tools.convert_time_to_age(build['started_at']))
+            data['time_event'] = time_event
+
+        elif build['state'] != 'created':
+            time_event = 'Finished {}'.format(self.__tools.convert_time_to_age(build['finished_at']))
+            data['time_event'] = time_event
+
+        return data
--- static/js/script.js
+++ static/js/script.js
@@ -1,25 +1,29 @@
-function resizePanelItems(){
-    var panel_top_height = 0.23;
-    var panel_middle_height = 0.56;
-    var panel_bottom_height = 0.21;
-    var event_details_height = 0.16*0.56;
-    var panel_height = parseInt($(".panel").height());
-    var repo_title_size = panel_height*panel_top_height*0.47;
-    var event_title_size = panel_height*panel_middle_height*0.16;
-    var event_details_text_size = panel_height*event_details_height*0.95;
-    var event_details_img_size = panel_height*event_details_height;
-    var event_details_margin = panel_height*event_details_height*0.6;
-    var panel_bottom_text = panel_height*panel_bottom_height*0.37;
+// update dashboard data
+function updateDashboard(){
+    $.ajax({
+        url: "/update",
+        success: function (data) {
+           $('#container').html(data);
+        },
+    });
+};
 
-    $(".panel-top a").css('font-size', repo_title_size);
-    $(".panel-middle .event-title").css('font-size', event_title_size);
-    $(".event-details p").css('font-size', event_details_text_size);
-    $(".event-details img").css('width', event_details_img_size);
-    $(".event-details").css('margin', event_details_margin);
-    $(".panel-bottom a").css('font-size', panel_bottom_text);
-    $(".panel-bottom p").css('font-size', panel_bottom_text);
-}
+$(document).ready(function(){
 
-$(window).resize(function () {
-    resizePanelItems();
+    $("#actions").on("shown.bs.modal", function (e) {
+        clearTimeout(timer);
+        var slug = e.relatedTarget.dataset.slug; 
+        $.ajax({
+            url: "/modal?slug=" + slug,
+            success: function (data) {
+                $('.modal-content').html(data);
+            },
+        });      
+    });
+
+    $("#actions").on("hidden.bs.modal", function(event){
+        $('.modal-content').html('<div class="modal-loading">Loading...</div>');
+        timer = setInterval(function(){updateDashboard();}, 5000);
+    });
+
 });
